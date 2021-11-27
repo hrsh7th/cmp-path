ba1fe4fd5142569357a841a00da26f0d4a392cff
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2021-11-27 12:53:04 +0200

    Avoid generating previews for device files

--- lua/cmp_path/init.lua
+++ lua/cmp_path/init.lua
@@ -100,7 +100,7 @@ local function lines_from(file, count)
   local bfile = assert(io.open(file, 'rb'))
   local first_k = bfile:read(1024)
   if first_k:find('\0') then
-	  return {'binary file'}
+    return {'binary file'}
   end
   local lines = {'```'}
   for line in first_k:gmatch("[^\r\n]+") do
@@ -111,15 +111,6 @@ local function lines_from(file, count)
   end
   lines[#lines + 1] = '```'
   return lines
-end
-
-local function try_get_lines(file, count)
-  status, ret = pcall(lines_from, file, count)
-  if status then
-    return ret
-  else
-    return nil
-  end
 end
 
 source._candidates = function(_, params, dirname, offset, callback)
@@ -211,7 +202,13 @@ end
 
 function source:resolve(completion_item, callback)
   if completion_item.kind == cmp.lsp.CompletionItemKind.File then
-    completion_item.documentation = try_get_lines(completion_item.data.path, defaults.max_lines)
+    local path = completion_item.data.path
+    if not vim.startswith(path, '/dev') then
+      local ok, preview_lines = pcall(lines_from, path, defaults.max_lines)
+      if ok then
+        completion_item.documentation = preview_lines
+      end
+    end
   end
   callback(completion_item)
 end
